0307ceebf5a93f02e97094c7b63770630cb31744
Tsio RJ <yyroist@gmail.com>
2022-03-18 16:45:10 +0100

    #7 - Création service Cart et ajout de Product au Cart

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Service\Cart;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Product;
@@ -18,39 +20,16 @@ use Symfony\Component\Translation\TranslatableMessage;
 class CartController extends AbstractController
 {
     /**
-     * Ajout d'un produit au panier.
+     * Chargement du panier
      *
-     * @param Product $product
+     * @param Request $request
+     * @param Cart $cart
      * @return Response
      */
-    public function addToCart(Product $product): Response
+    #[Route('/', name: 'app_cart', methods: ['GET', 'POST'])]
+    public function index(Request $request, Cart $cart): Response
     {
-        // Création du formulaire
-        $form = $this->createFormBuilder()
-            ->add('product', HiddenType::class, ['data' => $product->getId()])
-            ->add('quantity', NumberType::class, [
-                'label' => false,
-                'scale' => 0,
-                'attr' => [
-                    'max' => $product->getQuantity(),
-                    'value' => 0
-                ]
-            ])
-            ->add('submit', SubmitType::class, [
-                'label' => new TranslatableMessage('cart.addToCart'),
-                'attr' => [
-                    'class' => 'primary-btn'
-                ]
-            ])
-            ->getForm()
-        ;
-
-        // todo: récupération de la quantité voulue et le produit concerné
-        // todo: vérifier la disponibilité deu produit
-        // todo: calcul du sous-total pour chaque produit ajouté
-
-        return $this->render('cart/_add_to_cart_form.html.twig', [
-            'form' => $form->createView()
-        ]);
+        dd($cart->getProducts());
+        return $this->render('cart/index.html.twig');
     }
 }
---
+++ src/Service/Cart.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Entity\Product;
+use Exception;
+use Symfony\Component\Translation\TranslatableMessage;
+
+/**
+ * Gestion du panier de l'utilisateur.
+ *
+ */
+class Cart
+{
+    /**
+     * Private attributes.
+     */
+    /** @var SessionInterface $session Interface permettant d'interagir avec les données de session */
+    private SessionInterface $session;
+
+
+    /**
+     * Constructeur.
+     */
+    public function __construct(RequestStack $stack)
+    {
+        $this->session = $stack->getSession();
+    }
+
+    /**
+     * Récupère la liste des produits du panier en cours.
+     *
+     * @return array
+     */
+    public function getProducts(): array
+    {
+        return $this->session->get('products', []);
+    }
+
+    /**
+     * Ajout d'un produit au panier.
+     *
+     * @param Product $product
+     * @param int $quantity
+     * @return void
+     * @throws Exception
+     */
+    public function addProduct(Product $product, int $quantity)
+    {
+        // Vérification de la quantité ajoutée
+        if ($quantity <= 0) {
+            throw new Exception(new TranslatableMessage('cart.emptyQty'));
+        }
+
+        // Récupération de la liste des produits du panier
+        $products = $this->getProducts();
+
+        // Recherche du produit que l'on veut ajouter
+        $isAlreadyInCart = false;
+        if (!empty($products)) {
+            foreach ($products as $elem) {
+                if ($elem['product'] === $product) {
+                    // Si le produit est déjà dans le panier, on incrémente uniquement la quantité de ce dernier
+                    $isAlreadyInCart = true;
+                    $elem['quantity'] += $quantity;
+
+                    break;
+                }
+            }
+        }
+
+        // Si non trouvé dans le panier, on crée la ligne
+        if (!$isAlreadyInCart) {
+            $products[] = [
+                'product' => $product,
+                'quantity' => $quantity
+            ];
+        }
+
+        $this->session->set('products', $products);
+    }
+
+    /**
+     * Suppression d'un produit du panier.
+     *
+     * @param Product $product
+     * @return void
+     */
+    public function removeProduct(Product $product)
+    {
+        $products = $this->getProducts();
+        if (!empty($products)) {
+            foreach ($products as $k => $elem) {
+                if ($elem['product'] === $product) {
+                    unset($products[$k]);
+                    $this->session->set('products', $products);
+                }
+            }
+        }
+    }
+}
