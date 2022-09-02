435f23f5b6de93f3bac27666d6aebfaf8aaae9ea
Yannis Lanista <yannis.lanista@hotmail.com>
2022-09-02 14:25:03 +0200

    Ajout des controllers

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/article')]
+class ArticleController extends AbstractController
+{
+    #[Route('/', name: 'app_blog', methods: ['GET'])]
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        return $this->render('article/index.html.twig', [
+            'articles' => $articleRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/detail/{slug}', name: 'app_article_detail')]
+    public function show(Article $article): Response
+    {
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+
+}
