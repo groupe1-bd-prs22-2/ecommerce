00d6ea28230338bae545e5f9164a3716b67e234e
Tsiory RAJOARIFERA <yyroist@gmail.com>
2022-10-18 17:21:37 +0200

    #22 - Récupération des informations récapitulatives du tableau de bord

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Order;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,13 +16,31 @@ class DashboardController extends AbstractController
     /**
      * Chargement du tableau de bord.
      *
+     * @param EntityManagerInterface $em
      * @return Response
      */
     #[Route('/', name: 'admin_dashboard_index')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        // Récupération des différentes informations
+        $orders = $em->getRepository(Order::class)->findBy(['status' => [
+            Order::STATUS_SHIPPED, Order::STATUS_PREPARATION, Order::STATUS_DELIVERED]
+        ]); // Total des ventes
+
+        // Récupération du chiffre d'affaires
+        $earnigs = 0;
+        foreach ($orders as $order) {
+            foreach ($order->getProducts() as $purchase) {
+                $earnigs += $purchase->getProduct()->getPrice() * $purchase->getQuantity();
+            }
+        }
+
         return $this->render('admin/dashboard/index.html.twig', [
             'controller_name' => 'Tableau de bord',
+            'sales' => count($orders),
+            'earnings' => $earnigs,
+            'orders' => $em->getRepository(Order::class)->count([]),
+            'users' => $em->getRepository(User::class)->count([])
         ]);
     }
 }
