40f6c6719284ec7f84f528c3a86e4f4ab6018142
Samir Yagoubi <Samiryagoubi75@gmail.com>
2022-10-20 15:42:04 +0200

    #20 Contact Back OK - Contact Admin OK

---
+++ src/Controller/Admin/ContactController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Contact;
+use App\Repository\ContactRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class ContactController extends AbstractController
+{
+    #[Route('/admin/contact', name: 'admin_contact_index')]
+    public function index(ContactRepository $contactRepository): Response
+    {
+        return $this->render('admin/contact/index.html.twig', [
+            'contacts' => $contactRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * Suppression d'une fiche support.
+     *
+     * @param Request $request
+     * @param Contact $contact
+     * @param ContactRepository $contactRepository
+     * @return Response
+     */
+    #[Route('/{id}', name: 'admin_contact_delete', methods: ['POST'])]
+    public function delete(Request $request, Contact $contact, ContactRepository $contactRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$contact->getId(), $request->request->get('_token'))) {
+            $contactRepository->remove($contact);
+        }
+
+        return $this->redirectToRoute('admin_contactgit _index', [], Response::HTTP_SEE_OTHER);
+    }
+}
